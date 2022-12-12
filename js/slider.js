19555b9c703c9de0076df7249ba323e590f0c6eb
StanislavSlobodenyuk <116057509+StanislavSlobodenyuk@users.noreply.github.com>
2022-12-12 20:12:03 +0200

    Додали новий розділ

--- js/slider.js
+++ js/slider.js
@@ -32,3 +32,57 @@ new Swiper('.swipper-review',{
    speed: 800,
 
 })
+
+new Swiper('.swiper-howDoWeWork',{
+    pagination: {
+        el: ".swiper-pagination",
+        // булети =============================================================
+        // перехід на слайд по кліку на пагінацію
+        clickable: true,
+        // динамічні булети
+        dynamicBullets: true,
+        // булети =============================================================
+    },
+    // увімкнути/вимкнути захват та рух слайдів мишею на ПК
+    simulateTouch: true,
+    // чутливість
+    touchRatio: 1,
+    // кут роботи властивості
+    touchAngle: 45,
+    // курсор при переміщені
+    grabCursor: true,
+    // перемикання при кліку на слайд
+    slideToClickedSlide: true,
+
+    // управління клавіатурою 
+    keyboard: {
+        // ввімкнути/вимкнути 
+        enabled: true,
+        // ввімкнути тільки тоді коли слайдер видно
+        onlyInViewport: true,
+        // ввімкнути керування за допомогою page up/down
+        pageUpDown: true,
+    },
+    // кількість слайдів для показу читатти там відключення функціоналу якщо слайдів менше чим порібно 
+    watchOverflow: true,
+
+   // швидкість перемикання слайдів
+   speed: 2000,
+   
+   breakpoints: {
+    320: {
+        slidesPerView: 1.2,
+        spaceBetween: 24,
+    },
+    500.98:{
+        spaceBetween: 0,
+    },
+    767: {
+        slidesPerView: 2.2,
+        spaceBetween: 40,
+    },
+    992: {
+        slidesPerView: 3.2,
+    },
+}
+})
